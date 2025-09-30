16539ca0d44dd7f4af1c87116634300e0850af02
Eilodon <bao.nt.1992@gmail.com>
2025-09-30 16:23:35 +0700

    chore: prepare for monorepo refactor (pre-move state)

--- native_core/core_native/src/lib.rs
+++ native_core/core_native/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod interfaces;
 pub mod ontology;
+pub mod aca_layer;
+pub mod fep_cell;
--- native_core/protocols_native/build.rs
+++ native_core/protocols_native/build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    prost_build::compile_protos(&["proto/ontology.proto"], &["proto/"]).unwrap();
+    prost_build::compile_protos(&["proto/ontology.proto", "proto/core.proto"], &["proto/"]).unwrap();
 }
